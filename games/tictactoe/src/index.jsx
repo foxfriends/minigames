60f2ae4cd8033ae0312aa066b1b42cb58d1789e0
Cameron Eldridge <cameldridge@gmail.com>
2021-12-11 00:14:27 -0500

    Avoid top level await

--- games/tictactoe/src/index.jsx
+++ games/tictactoe/src/index.jsx
@@ -4,15 +4,19 @@ import App from "./App";
 import "./index.css";
 import validate from "./util/jwt";
 
-try {
-  const params = new URLSearchParams(window.location.search);
-  const gameId = params.get("game_id");
-  const { userId } = await validate(params.get("token"));
-  render(
-    <App gameId={gameId} userId={userId} />,
-    document.querySelector("#app"),
-  );
-} catch (error) {
-  console.error(error);
-  render(<div>You don't have access.</div>, document.querySelector("#app"));
+async function main() {
+  try {
+    const params = new URLSearchParams(window.location.search);
+    const gameId = params.get("game_id");
+    const { userId } = await validate(params.get("token"));
+    render(
+      <App gameId={gameId} userId={userId} />,
+      document.querySelector("#app"),
+    );
+  } catch (error) {
+    console.error(error);
+    render(<div>You don't have access.</div>, document.querySelector("#app"));
+  }
 }
+
+main();
