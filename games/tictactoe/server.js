ff85f76163ae9c5865ae1f3064015d8d987d78c9
Cameron Eldridge <cameldridge@gmail.com>
2021-12-11 11:52:38 -0500

    Improve error message when main server is not reachable from minigame server. Closes #17

--- games/tictactoe/server.js
+++ games/tictactoe/server.js
@@ -12,6 +12,24 @@ const {
   API_KEY,
 } = process.env;
 
+async function tryFetch(...params) {
+  try {
+    const response = await fetch(...params);
+    if (response.status !== 200) {
+      const { code, message, data } = await response.json();
+      throw new Error(message);
+    }
+    return await response.json();
+  } catch (error) {
+    if (error.name === 'FetchError') {
+      console.log(`${chalk.red('Error')}: Could not connect to the main server at ${chalk.cyan(VITE_API_URL)}. Ensure the server is running and that the ${chalk.green('VITE_API_URL')} environment variable is set correctly.`);
+    } else {
+      console.error(error.message);
+    }
+    process.exit(1);
+  }
+}
+
 export async function createServer() {
   if (NODE_ENV === "development") {
     const vite = await createViteServer(config);
@@ -23,26 +41,27 @@ export async function createServer() {
   }
 }
 
-createServer().then(async (app) => {
-  const server = await app.listen(PORT, async () => {
-    console.log(`${chalk.yellow("tictactoe")} is running on port ${PORT}`);
-    await fetch(`${VITE_API_URL}/games/tictactoe`, {
-      method: "POST",
-      headers: { Authorization: `Bearer ${API_KEY}` },
-      body: JSON.stringify({ url: PUBLIC_URL }),
+createServer()
+  .then(async (app) => {
+    const server = await app.listen(PORT, async () => {
+      console.log(`${chalk.yellow("tictactoe")} is running on port ${PORT}`);
+      await tryFetch(`${VITE_API_URL}/games/tictactoe`, {
+        method: "POST",
+        headers: { Authorization: `Bearer ${API_KEY}` },
+        body: JSON.stringify({ url: PUBLIC_URL }),
+      });
     });
+
+    async function shutdown() {
+      console.log(`Shutting down ${chalk.yellow("tictactoe")}`);
+      await tryFetch(`${VITE_API_URL}/games/tictactoe`, {
+        method: "DELETE",
+        headers: { Authorization: `Bearer ${API_KEY}` },
+      });
+      server.close();
+      process.exit(0);
+    }
+
+    process.on("SIGTERM", shutdown);
+    process.on("SIGINT", shutdown);
   });
-
-  async function shutdown() {
-    console.log(`Shutting down ${chalk.yellow("tictactoe")}`);
-    await fetch(`${VITE_API_URL}/games/tictactoe`, {
-      method: "DELETE",
-      headers: { Authorization: `Bearer ${API_KEY}` },
-    });
-    server.close();
-    process.exit(0);
-  }
-
-  process.on("SIGTERM", shutdown);
-  process.on("SIGINT", shutdown);
-});
