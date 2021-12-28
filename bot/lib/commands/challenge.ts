426d1eeb28058d0e938ced46c67495cb131dd548
Cameron Eldridge <cameldridge@gmail.com>
2021-12-27 22:42:28 -0500

    Bot restricts who can click the accept/reject buttons. Closes #33

--- bot/lib/commands/challenge.ts
+++ bot/lib/commands/challenge.ts
@@ -4,7 +4,9 @@ import {
   ApplicationCommandTypes,
   ButtonStyles,
   DiscordenoInteraction,
+  DiscordenoMessage,
   InteractionResponseTypes,
+  MessageComponents,
   MessageComponentTypes,
 } from "../../deps/discordeno.ts";
 import { userOption } from "./utils.ts";
@@ -38,6 +40,12 @@ function createChallenge(params: api.CreateChallenge): Task {
       throw await response.json();
     }
   });
+}
+
+function alreadyResponded(message: DiscordenoMessage, userId: bigint): boolean {
+  if (message.authorId === userId) return true;
+  // A sketchy way to check but... what more can we do than check message content?
+  return message.content.includes(`<@${userId}> has`);
 }
 
 export const challenge: Command = {
@@ -124,14 +132,38 @@ export const challenge: Command = {
     });
   },
 
-  handleComponentInteraction({ message, data }: DiscordenoInteraction): Task {
+  handleComponentInteraction({ message, user, data }: DiscordenoInteraction): Task {
     // deno-lint-ignore no-explicit-any
     return task(async function* (): AsyncGenerator<Task, void, any> {
+      const participants: bigint[] = message!.mentionedUserIds ?? [];
+
+      if (!participants.includes(user.id)) {
+        yield respond({
+          type: InteractionResponseTypes.ChannelMessageWithSource,
+          private: true,
+          data: {
+            content: "You are not a participant in this game. Why not challenge someone yourself?",
+          },
+        });
+        return;
+      }
+
+      if (alreadyResponded(message!, user.id)) {
+        yield respond({
+          type: InteractionResponseTypes.ChannelMessageWithSource,
+          private: true,
+          data: {
+            content: "You have already responded to this challenge. Wait for the others!",
+          },
+        });
+        return;
+      }
+
       if (data!.customId === "challenge_rejected") {
         yield respond({
           type: InteractionResponseTypes.UpdateMessage,
           data: {
-            content: `${message!.content}\n\nThe challenge was **rejected**... ${shame()}`,
+            content: `${message!.content}\n\n<@${user.id}> has **rejected**... ${shame()}`,
             components: [],
           },
         });
@@ -141,19 +173,28 @@ export const challenge: Command = {
       const gameId = data!.customId!;
       const url: string = yield getGameUrl(gameId);
 
+      const allResponded = participants
+        .filter((participant) => participant !== user.id)
+        .every((participant) => alreadyResponded(message!, participant));
+
+      let components: MessageComponents | undefined;
+      if (allResponded) {
+        components = [{
+          type: 1,
+          components: [{
+            type: MessageComponentTypes.Button,
+            style: ButtonStyles.Link,
+            label: "Let's Play",
+            url,
+          }],
+        }];
+      }
+
       yield respond({
         type: InteractionResponseTypes.UpdateMessage,
         data: {
-          content: `${message!.content}\n\nThe challenge was **accepted**.`,
-          components: [{
-            type: 1,
-            components: [{
-              type: MessageComponentTypes.Button,
-              style: ButtonStyles.Link,
-              label: "Let's Play",
-              url,
-            }],
-          }],
+          content: `${message!.content}\n\n<@${user.id}> has **accepted**.`,
+          components,
         },
       });
     });
