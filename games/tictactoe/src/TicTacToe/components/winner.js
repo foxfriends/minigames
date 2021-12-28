4824ca5336eed72e5a62c10d1a17e8de2844c4a9
Cameron Eldridge <cameldridge@gmail.com>
2021-12-27 22:42:45 -0500

    npm run fmt

--- games/tictactoe/src/TicTacToe/components/winner.js
+++ games/tictactoe/src/TicTacToe/components/winner.js
@@ -5,7 +5,9 @@ export default function useTicTacToeWinner() {
   useWinner((gameState) => {
     const winningCells = win(gameState);
     if (!winningCells) {
-      if (gameState.cells.every((cell) => cell.value)) { return null; }
+      if (gameState.cells.every((cell) => cell.value)) {
+        return null;
+      }
       return;
     }
     const winningSymbol = gameState.cells[winningCells[0]].value;
