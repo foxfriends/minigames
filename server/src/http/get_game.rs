ef202127f00c2a4f9175b39e7d6877ce405714e3
Cameron Eldridge <cameldridge@gmail.com>
2021-12-27 20:49:28 -0500

    cargo fmt

--- server/src/http/get_game.rs
+++ server/src/http/get_game.rs
@@ -1,7 +1,7 @@
 use super::response::Response;
 use crate::game::{Game, GameId, GameName, GameParticipant};
-use crate::user::UserId;
 use crate::postgres::PgPool;
+use crate::user::UserId;
 use rocket::serde::json::Json;
 use rocket::State;
 use serde::{Deserialize, Serialize};
