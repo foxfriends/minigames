33db1318d9975b49cf5818477822469a77d0e4a9
Cameron Eldridge <cameldridge@gmail.com>
2021-12-30 20:37:44 -0500

    npm run fmt

--- packages/server-express/.eslintrc.cjs
+++ packages/server-express/.eslintrc.cjs
@@ -10,6 +10,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
+    "prettier",
   ],
 };
