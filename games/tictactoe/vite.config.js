1c99bd54fdaa95a490210f4a98e89a6c522eb42c
Cameron Eldridge <cameldridge@gmail.com>
2022-01-04 00:19:57 -0500

    Add an environment variable to configure the public base URL for tictactoe

--- games/tictactoe/vite.config.js
+++ games/tictactoe/vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
+import { config } from "dotenv";
+
+config()
+
+const base = process.env.PUBLIC_BASE_PATH;
 
 export default defineConfig({
   server: { hmr: false },
+  base,
 });
