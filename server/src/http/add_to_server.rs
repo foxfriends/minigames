ea0236529971d9a35232d3f7656b9ad5694c0ff7
Cameron Eldridge <cameldridge+git@gmail.com>
2022-01-14 00:36:22 -0500

    Update add_to_server.rs

--- server/src/http/add_to_server.rs
+++ server/src/http/add_to_server.rs
@@ -3,5 +3,5 @@ use rocket::response::Redirect;
 #[rocket::get("/install")]
 pub async fn add_to_server() -> Redirect {
     let client_id = crate::env::discord_client_id();
-    Redirect::to(format!("https://discord.com/api/oauth2/authorize?client_id={}&scope=bot&permissions=10240&scope=applications.commands", client_id))
+    Redirect::to(format!("https://discord.com/api/oauth2/authorize?client_id={}&scope=bot%20applications.commands&permissions=10240", client_id))
 }
