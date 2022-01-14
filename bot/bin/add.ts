750d3c75586aa36583a436de0f79668099287c15
Cameron Eldridge <cameldridge+git@gmail.com>
2022-01-13 23:09:39 -0500

    Update add.ts

--- bot/bin/add.ts
+++ bot/bin/add.ts
@@ -1,5 +1,5 @@
 import { DISCORD_CLIENT_ID } from "./bot/config.ts";
 
 console.log(
-  `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&scope=bot&permissions=10240&scope=applications.commands`,
+  `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&scope=bot%20applications.commands&permissions=10240`,
 );
