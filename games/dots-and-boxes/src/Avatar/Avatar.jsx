fc68251fe3e4b568c3b6ff7d70f5697bb6ba3ac2
Cameron Eldridge <cameldridge@gmail.com>
2022-01-29 17:02:28 -0500

    Fix avatar showing first character of ID instead of username

--- games/dots-and-boxes/src/Avatar/Avatar.jsx
+++ games/dots-and-boxes/src/Avatar/Avatar.jsx
@@ -6,14 +6,17 @@ import { avatar } from "./Avatar.module.css";
 export default function Avatar({ id }) {
   const { loading, players } = useGameInfo();
   const { size } = useDotsAndBoxes();
-  const avatarHash = players.find((player) => player.id === id)?.avatar;
+  const player = players.find((player) => player.id === id);
+  if (!player) {
+    return null;
+  }
   const grid = BOARD_SIZE / size;
-  const url = `https://cdn.discordapp.com/avatars/${id}/${avatarHash}.png?size=128`;
+  const url = `https://cdn.discordapp.com/avatars/${id}/${player.avatar}.png?size=128`;
   return (
     <img
       className={avatar}
       src={url}
-      alt={id[0]}
+      alt={player.username[0]}
       style={{ width: grid * 0.6, height: grid * 0.6 }}
     />
   );
